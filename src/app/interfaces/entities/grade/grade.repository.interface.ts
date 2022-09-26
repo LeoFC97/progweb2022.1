bb2973eb1295ed90abeab850a8c3704ccd2fc428
leonardo <leonardo.fc2@outlook.com>
2022-09-26 10:00:44 -0300

    refactor: change name to verbosity interface

--- src/app/interfaces/entities/grade/grade.repository.interface.ts
+++ src/app/interfaces/entities/grade/grade.repository.interface.ts
@@ -1,8 +1,8 @@
 import RepositoryInterface from '../../repository';
 import Grade, { CreateGradeBodyData } from './grade';
 
-export default interface GradeRepository extends RepositoryInterface {
+export default interface GradeRepositoryInterface extends RepositoryInterface {
   create(gradeToBeCreated: CreateGradeBodyData): Promise<Grade>;
   getById(gradeId: string): Promise<Grade>;
-  getByCourseName(courseName: string, coursePeriod: string): Promise<Grade>;
+  getByCourseNameAndPeriod(courseName: string, coursePeriod: string): Promise<Grade>;
 }
